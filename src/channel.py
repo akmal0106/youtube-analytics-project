dab906ad6bce962076d5276eb77c534112c293e0
Akmal Sharipov <akmal.sharipov2002@mail.ru>
2023-10-03 10:54:38 +0500

    add homework-1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,17 @@
+import requests
+import json
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    def __init__(self, channel_id):
+        self.channel_id = channel_id
 
-    def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+    def get_channel_info(self):
+        url = f"https://www.googleapis.com/youtube/v3/channels?id={self.channel_id}&part=snippet,contentDetails,statistics&key=AIzaSyBNsbt4e1yy1uVY1GMZoUi31VSE3sSs108"
+        response = requests.get(url)
+        json_data = json.loads(response.text)
+        return json.dumps(json_data, indent=2, ensure_ascii=False)
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+    def print_info(self):
+        channel_info = self.get_channel_info()
+        return channel_info
