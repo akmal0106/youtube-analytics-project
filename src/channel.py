b2fddf5b5f8e231d0fe3edbdfe950fcc7defc49b
Akmal Sharipov <akmal.sharipov2002@mail.ru>
2023-10-03 20:19:12 +0500

    испрваил ошибки homework-1

--- src/channel.py
+++ src/channel.py
@@ -2,14 +2,11 @@ from googleapiclient.discovery import build
 import os
 import json
 class Channel:
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
     def __init__(self, channel_id):
         self.channel_id = channel_id
 
-    def get_service(self):
-        api_key: str = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        return youtube
-
     def print_info(self):
-        dict_to_print = self.get_service().channels().list(id=self.channel_id, part='snippet, statistics').execute()
+        dict_to_print = self.youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()
         return json.dumps(dict_to_print, indent=2, ensure_ascii=False)
